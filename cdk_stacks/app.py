ffd13350f505d9a294a7d7e1201468994ea59c01
ksmin23 <sonagy23@gmail.com>
2023-10-01 23:15:04 +0900

    initial commit

---
+++ cdk_stacks/app.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from rag_with_aos import (
+  VpcStack,
+  BastionHostEC2InstanceStack,
+  OpenSearchStack,
+  SageMakerStudioStack,
+  SageMakerLambdaLayerStack,
+  JumpStartModelDeployLambdaStack,
+  SageMakerEndpointIAMRoleStack,
+  CustomResourceStack
+)
+
+APP_ENV = cdk.Environment(
+  account=os.environ["CDK_DEFAULT_ACCOUNT"],
+  region=os.environ["CDK_DEFAULT_REGION"]
+)
+
+app = cdk.App()
+
+vpc_stack = VpcStack(app, 'RAGHaystackVpcStack',
+  env=APP_ENV)
+
+ops_stack = OpenSearchStack(app, 'RAGHaystackOpenSearchStack',
+  vpc_stack.vpc,
+  env=APP_ENV
+)
+ops_stack.add_dependency(vpc_stack)
+
+bastion_host = BastionHostEC2InstanceStack(app, 'RAGHaystackBastionHost',
+  vpc_stack.vpc,
+  ops_stack.ops_client_sg,
+  env=APP_ENV
+)
+bastion_host.add_dependency(ops_stack)
+
+sm_studio_stack = SageMakerStudioStack(app, 'RAGHaystackSageMakerStudioStack',
+  vpc_stack.vpc,
+  ops_stack.ops_client_sg.security_group_id,
+  # ops_stack.ops_client_sg_id,
+  env=APP_ENV
+)
+sm_studio_stack.add_dependency(bastion_host)
+
+lambda_layer_stack = SageMakerLambdaLayerStack(app, "RAGHaystackSMPySDKLambdaLayerStack",
+  env=APP_ENV)
+lambda_layer_stack.add_dependency(sm_studio_stack)
+
+sagemaker_endpoint_iam_role_stack = SageMakerEndpointIAMRoleStack(app, "RAGHaystackSMEndpointRoleStack",
+  env=APP_ENV)
+sagemaker_endpoint_iam_role_stack.add_dependency(lambda_layer_stack)
+
+lambda_function_stack = JumpStartModelDeployLambdaStack(app, "RAGHaystackSMJSModelDeployLambdaStack",
+  lambda_layer_stack.lambda_layer,
+  sagemaker_endpoint_iam_role_arn=sagemaker_endpoint_iam_role_stack.sagemaker_endpoint_role_arn,
+  env=APP_ENV)
+lambda_function_stack.add_dependency(sagemaker_endpoint_iam_role_stack)
+
+custom_resource_stack = CustomResourceStack(app, "RAGHaystackSMJSModelEndpointStack",
+  lambda_function_stack.lambda_function_arn,
+  env=APP_ENV)
+custom_resource_stack.add_dependency(lambda_function_stack)
+
+app.synth()
---
+++ cdk_stacks/rag_with_aos/sagemaker_endpoint_iam_role.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import random
+import string
+
+import aws_cdk as cdk
+
+from aws_cdk import (
+  Stack,
+  aws_iam
+)
+from constructs import Construct
+
+random.seed(47)
+
+
+class SageMakerEndpointIAMRoleStack(Stack):
+
+  def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
+
+    sagemaker_endpoint_role = aws_iam.Role(self, 'SageMakerEndpointRole',
+      role_name='AmazonSageMakerEndpointRole-{suffix}'.format(suffix=''.join(random.choices((string.digits), k=5))),
+      assumed_by=aws_iam.ServicePrincipal('sagemaker.amazonaws.com'),
+      path='/',
+      managed_policies=[
+        aws_iam.ManagedPolicy.from_aws_managed_policy_name('IAMReadOnlyAccess'),
+        aws_iam.ManagedPolicy.from_aws_managed_policy_name('AmazonSageMakerReadOnly'),
+        aws_iam.ManagedPolicy.from_aws_managed_policy_name('AmazonS3ReadOnlyAccess'),
+        aws_iam.ManagedPolicy.from_aws_managed_policy_name('CloudWatchLogsFullAccess'),
+        aws_iam.ManagedPolicy.from_aws_managed_policy_name('service-role/AWSAppRunnerServicePolicyForECRAccess'),
+      ]
+    )
+
+    self.sagemaker_endpoint_role_arn = sagemaker_endpoint_role.role_arn
+
+    cdk.CfnOutput(self, 'SageMakerEndpointIAMRoleName', value=sagemaker_endpoint_role.role_name,
+      export_name=f'{self.stack_name}-RoleName')
+    cdk.CfnOutput(self, 'SageMakerEndpointIAMRoleArn', value=self.sagemaker_endpoint_role_arn,
+      export_name=f'{self.stack_name}-RoleArn')
